58605726d38678c5e4eb113f02d502c5b693bb47
Olivia Lucca Fraser <lucca@mushussu.local>
2024-09-03 13:05:02 -0300

    tweaked it for gbnf syntax

--- bnf-mode.el
+++ bnf-mode.el
@@ -1,11 +1,11 @@
-;;; bnf-mode.el --- Major mode for editing BNF grammars. -*- lexical-binding: t; -*-
+;;; gbnf-mode.el --- Major mode for editing GBNF grammars. -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2019-2022 Free Software Foundation, Inc
 
 ;; Author: Serghei Iakovlev <egrep@protonmail.ch>
 ;; Maintainer: Serghei Iakovlev <egrep@protonmail.ch>
 ;; Version: 0.4.5
-;; URL: https://github.com/sergeyklay/bnf-mode
+;; URL: https://github.com/oblivia-simplex/gbnf-mode
 ;; Keywords: languages
 ;; Package-Requires: ((cl-lib "0.5") (emacs "25.1"))
 ;; Revision: $Format:%h (%cD %d)$
@@ -27,8 +27,8 @@
 
 ;;; Commentary:
 
-;;   BNF Mode is a GNU Emacs major mode for editing BNF grammars.  Presently it
-;; provides basic syntax and font-locking for BNF files.  BNF notation is
+;;   GBNF Mode is a GNU Emacs major mode for editing GBNF grammars.  Presently it
+;; provides basic syntax and font-locking for GBNF files.  GBNF notation is
 ;; supported exactly form as it was first announced in the ALGOL 60 report.
 
 ;;; Code:
@@ -43,40 +43,39 @@
 ;;;; Customization
 
 ;;;###autoload
-(defgroup bnf nil
-  "Major mode for editing BNF grammars."
-  :tag "BNF"
-  :prefix "bnf-"
+(defgroup gbnf nil
+  "Major mode for editing GBNF grammars."
+  :tag "GBNF"
+  :prefix "gbnf-"
   :group 'languages
-  :link '(url-link :tag "GitHub Page" "https://github.com/sergeyklay/bnf-mode")
-  :link '(emacs-commentary-link :tag "Commentary" "bnf-mode"))
+  :link '(url-link :tag "GitHub Page" "https://github.com/sergeyklay/gbnf-mode")
+  :link '(emacs-commentary-link :tag "Commentary" "gbnf-mode"))
 
 
 ;;;; Specialized rx
 
 (eval-when-compile
-  (defconst bnf-rx-constituents
-    `((bnf-rule-name . ,(rx (and
-                             (1+ (or alnum digit))
-                             (0+ (or alnum digit
-                                     (in "!\"#$%&'()*+,-./:;=?@[]^_`{|}~")
-                                     (in " \t"))))))
-    "Additional special sexps for `bnf-rx'."))
+  (defconst gbnf-rx-constituents
+    `((gbnf-rule-name . ,(rx (and
+                              (1+ (or alnum digit))
+                              (0+ (or alnum digit
+                                      (in "_-"))))
+    "Additional special sexps for `gbnf-rx'."))
 
-  (defmacro bnf-rx (&rest sexps)
-     "BNF-specific replacement for `rx'.
+  (defmacro gbnf-rx (&rest sexps)
+     "GBNF-specific replacement for `rx'.
 
 In addition to the standard forms of `rx', the following forms
 are available:
 
-`bnf-rule-name'
-      Any valid BNF rule name.  This rule was obtained by studying
-      ALGOL 60 report, where the BNF was officially announced.
-      Please note: This rule is not suitable for ABNF or EBNF
+`gbnf-rule-name'
+      Any valid GBNF rule name.  This rule was obtained by studying
+      ALGOL 60 report, where the GBNF was officially announced.
+      Please note: This rule is not suitable for AGBNF or EGBNF
       (see URL `https://www.masswerk.at/algol60/report.htm').
 
 See `rx' documentation for more information about REGEXPS param."
-     (let ((rx-constituents (append bnf-rx-constituents rx-constituents)))
+     (let ((rx-constituents (append gbnf-rx-constituents rx-constituents)))
        (rx-to-string (cond ((null sexps) (error "No regexp is provided"))
                            ((cdr sexps)  `(and ,@sexps))
                            (t            (car sexps)))
@@ -85,43 +84,43 @@ See `rx' documentation for more information about REGEXPS param."
 
 ;;;; Font Locking
 
-(defvar bnf-font-lock-keywords
+(defvar gbnf-font-lock-keywords
   `(
     ;; LHS nonterminals may be preceded
     ;; by an unlimited number of spaces
-    (,(bnf-rx (and line-start
+    (,(gbnf-rx (and line-start
                    (0+ space)
-                   "<"
-                   (group bnf-rule-name)
-                   ">"
+                   (group gbnf-rule-name)
                    (0+ space)
                    "::="))
      1 font-lock-function-name-face)
     ;; Other nonterminals
-    (,(bnf-rx (and (0+ space)
-                   "<"
-                   (group bnf-rule-name)
-                   ">"
+    (,(gbnf-rx (and (0+ space)
+                   (group gbnf-rule-name)
                    (0+ space)))
      1 font-lock-builtin-face)
     ;; “may expand into” symbol
-    (,(bnf-rx (and symbol-start
+    (,(gbnf-rx (and symbol-start
                    (group "::=")
                    symbol-end))
      1 font-lock-constant-face)
     ;; Alternatives
-    (,(bnf-rx (and (0+ space)
+    (,(gbnf-rx (and (0+ space)
                    symbol-start
                    (group "|")
                    symbol-end
                    (0+ space)))
-     1 font-lock-warning-face))
-  "Font lock BNF keywords for BNF Mode.")
+     1 font-lock-warning-face)
+    ;; terminal strings
+    (,(gbnf-rx "\""
+               (not (any "\""))
+               "\"")))
+  "Font lock GBNF keywords for GBNF Mode.")
 
 
 ;;;; Syntax
 
-(defvar bnf-mode-syntax-table
+(defvar gbnf-mode-syntax-table
   (let ((table (make-syntax-table)))
     ;; FIXME: "_" doesn't mean "symbol" but "symbol constituent".
     ;; I.e. the settings below mean that Emacs will consider "a:b=(c" as one
@@ -135,47 +134,42 @@ See `rx' documentation for more information about REGEXPS param."
     ;; Treat | as a symbol
     (modify-syntax-entry ?\| "_" table)
 
-    ;; In BNF there are no strings
-    ;; so treat ' and " as a symbols
-    (modify-syntax-entry ?\" "_" table)
-    (modify-syntax-entry ?\' "_" table)
-
-    ;; In BNF there are no grouping
+    ;; In GBNF there are no grouping
     ;; brackets except angle ones
-    (modify-syntax-entry ?\( "_" table)
-    (modify-syntax-entry ?\) "_" table)
     (modify-syntax-entry ?\{ "_" table)
     (modify-syntax-entry ?\} "_" table)
-    (modify-syntax-entry ?\[ "_" table)
-    (modify-syntax-entry ?\] "_" table)
 
     ;; Group angle brackets
     (modify-syntax-entry ?\< "(>" table)
     (modify-syntax-entry ?\> ")<" table)
 
-    ;; Comments are begins with “;” and ends with “\n”
-    (modify-syntax-entry ?\; "<" table)
+    ;; Group square brackets
+    (modify-syntax-entry ?\[ "(]" table)
+    (modify-syntax-entry ?\] ")[" table)
+
+    ;; Comments are begins with “#” and ends with “\n”
+    (modify-syntax-entry ?\# "<" table)
     (modify-syntax-entry ?\n ">" table)
 
     table)
-  "Syntax table in use in `bnf-mode' buffers.")
+  "Syntax table in use in `gbnf-mode' buffers.")
 
 
 ;;;; Initialization
 
 ;;;###autoload
-(define-derived-mode bnf-mode prog-mode "BNF"
-  "A major mode for editing BNF grammars.
+(define-derived-mode gbnf-mode prog-mode "GBNF"
+  "A major mode for editing GBNF grammars.
 
-\\{bnf-mode-map}
+\\{gbnf-mode-map}
 
-Turning on BNF Mode calls the value of `prog-mode-hook' and then of
-`bnf-mode-hook', if they are non-nil."
-  :syntax-table bnf-mode-syntax-table
+Turning on GBNF Mode calls the value of `prog-mode-hook' and then of
+`gbnf-mode-hook', if they are non-nil."
+  :syntax-table gbnf-mode-syntax-table
 
   ;; Comments setup
   (setq-local comment-use-syntax nil)
-  (setq-local comment-start "; ")
+  (setq-local comment-start "# ")
   (setq-local comment-end "")
   (setq-local comment-start-skip "\\(?:\\(\\W\\|^\\);+\\)\\s-+")
 
@@ -183,20 +177,20 @@ Turning on BNF Mode calls the value of `prog-mode-hook' and then of
   (setq font-lock-defaults
         '(
           ;; Keywords
-          bnf-font-lock-keywords
+          gbnf-font-lock-keywords
           ;; keywords-only
           nil
           ;; According to RFC5234 rule names are case insensitive.
           ;; The names <rulename>, <Rulename>, <RULENAME>, and <rUlENamE>
           ;; all refer to the same rule.  As far as is known, this doesn't
-          ;; conflict with original BNF version
+          ;; conflict with original GBNF version
           ;; (see URL `https://tools.ietf.org/html/rfc5234')
           t)))
 
-;; Invoke bnf-mode when appropriate
+;; Invoke gbnf-mode when appropriate
 
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.bnf\\'" . bnf-mode))
+(add-to-list 'auto-mode-alist '("\\.gbnf\\'" . gbnf-mode))
 
-(provide 'bnf-mode)
-;;; bnf-mode.el ends here
+(provide 'gbnf-mode)
+;;; gbnf-mode.el ends here
